16c34cf07aad65bbd80bc0cfc1470bb4e8c7e9b7
m9993 <muntasiralam9993@gmail.com>
2026-07-01 04:14:42 +0600

    3d avatar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,15 +19,16 @@ export default function Hero(props: HeroType) {
       }
     >
 
-      <Reveal animation='rotate'>
+      {/* <Reveal animation='rotate'>
         <Avatar image={data.profileImage} />
-      </Reveal>
+      </Reveal> */}
+      <Avatar images={data.profileImages} />
 
       <Reveal animation='zoom' delay={0.8}><h1 className="font-mono mt-7 text-xl md:text-4xl">{data.name}</h1></Reveal>
 
       <div
         className="mt-1 w-80 md:w-2/4 md:mt-3">
-        <Reveal animation='blur' delay={1} duration={1}>
+        <Reveal animation='fade' delay={1.5} duration={5}>
           <p className="text-center text-xs md:text-base">
             {data.subTitle}
           </p>
